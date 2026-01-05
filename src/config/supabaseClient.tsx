169865a995da5eb4de34d9a66a01f1ed68aa6188
Eric <kaiguaeric@gmail.com>
2026-01-05 09:05:20 +0300

    Removed node modules and deleted frontend files in root directory

--- src/config/supabaseClient.tsx
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = import.meta.env.VITE_KEJANI_PROJECT_URL as string
-const supabaseKey = import.meta.env.VITE_KEJANI_API_KEY as string
-
-const supabase = createClient(supabaseUrl, supabaseKey)
-
-export default supabase
